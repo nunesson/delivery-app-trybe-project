5de15b4d7febba97adbfa4c9b84bce095bcd3d2c
João Matheus Silva Franca <matheussfranca@live.com>
2023-03-29 17:06:07 -0300

    Concluído: Requisito 01

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import Login from './Pages/Login';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 
 function App() {
   return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
-    </div>
+    <Switch>
+      <Route exact path="/" render={ () => <Redirect to="/login" /> } />
+      <Route exact path="/login" component={ Login } />
+    </Switch>
   );
 }
 
