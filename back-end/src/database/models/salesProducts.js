3a967c08aea4ed4ca94d25027d0384786aef86af
Anderson <quatroovos@gmail.com>
2023-03-29 16:07:31 -0300

    finalizando config sequelize

--- back-end/src/database/models/salesProducts.js
+++ back-end/src/database/models/salesProducts.js
@@ -1,35 +1,26 @@
 const salesProducts = (sequelize, DataTypes) => {
     const salesProducts = sequelize.define(
-      'sales_products',
-      {
-        id: {
-          allowNull: false,
-          autoIncrement: true,
-          primaryKey: true,
-          type: DataTypes.INTEGER
+        'salesProducts',
+        {
+            saleId: {
+                allowNull: false,
+                type: DataTypes.INTEGER
+            },
+            productId: {
+                allowNull: false,
+                type: DataTypes.INTEGER,
+            },
+            quantity: {
+                allowNull: false,
+                type: DataTypes.INTEGER,
+            },
         },
-        saleId: {
-          allowNull: false,
-          type: DataTypes.INTEGER
-        },
-        productId: {
-          allowNull: false,
-          type: DataTypes.INTEGER,
-        },
-        quantity: {
-          allowNull: false,
-          type: DataTypes.NUMBER
-        },
-        
-      },
-      {
-        tableName: 'sales_products',
-        underscored: true,
-        timestamps: false,
-      });
-      
+        {
+            tableName: 'salesProducts',
+            underscored: true,
+            timestamps: false,
+        });
     return salesProducts;
-  };
-  
-  
-  module.exports = salesProducts;
+};
+
+module.exports = salesProducts;
