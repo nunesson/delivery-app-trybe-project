10a921aa537793d2617713fc9659bf9b6de94f75
esdras <esdras12thiago@gmail.com>
2023-03-30 19:10:24 -0300

    Arrumando pastas do backend e deixar axios dinamico

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -2,7 +2,6 @@ const cors = require('cors');
 const express = require('express');
 const { error } = require('./Middlewares/Error');
 const routers = require('./Router/Router');
-const { aproveUsers } = require('../database/controller/login.controller');
 
 const app = express();
 
@@ -15,7 +14,5 @@ app.use(routers);
 app.use(error);
 
 app.use(express.json());
-
-app.post('/login', aproveUsers);
 
 module.exports = app;
---
+++ front-end/src/Axios/AxiosRoutes.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+
+const genericRoutes = async (route, data, type) => {
+  switch (type) {
+  case 'post':
+    try {
+      const response = await axios.post(`http://localhost:3001/${route}`, data);
+      return response;
+    } catch (error) {
+      const { status } = error.response;
+      return { status };
+    }
+  case 'get':
+    try {
+      const response = await axios.get(`http://localhost:3001/${route}`);
+      return response;
+    } catch (error) {
+      const { status } = error.response;
+      return { status };
+    }
+  case 'put':
+    try {
+      const response = await axios.put(`http://localhost:3001/${route}`, data);
+      return response;
+    } catch (error) {
+      const { status } = error.response;
+      return { status };
+    }
+  default:
+    break;
+  }
+};
+
+module.exports = {
+  genericRoutes,
+};
