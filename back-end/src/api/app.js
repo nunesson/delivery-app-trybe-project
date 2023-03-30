bd5198fa165fc5d2bfaf830c0dd30b5f5c77136f
João Matheus Silva Franca <matheussfranca@live.com>
2023-03-30 18:36:14 -0300

    Corrigindo erro de lint

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -2,7 +2,6 @@ const cors = require('cors');
 const express = require('express');
 const { error } = require('./Middlewares/Error');
 const routers = require('./Router/Router');
-const cors = require('cors');
 const { aproveUsers } = require('../database/controller/login.controller');
 
 const app = express();
