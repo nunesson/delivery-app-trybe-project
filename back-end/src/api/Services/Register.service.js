875a1132064f3198aa78aaa808fab1e0391748d1
VitorBarrioni <vitor_teixeira90@hotmail.com>
2023-03-30 17:17:55 -0300

    req 09

--- back-end/src/api/Services/Register.service.js
+++ back-end/src/api/Services/Register.service.js
@@ -1,11 +1,18 @@
 const md5 = require('md5');
+const { Op } = require('sequelize');
 const { users } = require('../../database/models');
 const { errorType } = require('../Middlewares/Error');
 
 const register = async ({ name, email, password }) => {
     const hash = md5(password);
+    const userExist = await users.findOne({ where: {
+        [Op.or]: [
+          { email },
+          { name },
+        ],
+      } });
+      errorType(userExist, 'usuario já existe', 409);
     const user = await users.create({ name, email, password: hash, role: 'customer' });
-    errorType(!user, 'dados invalidos', 404);
     return user.dataValues;
 };
 
