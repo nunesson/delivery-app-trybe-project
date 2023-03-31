57c952a8ba8d8bb8bcda5ee892b84786e15f6282
João Matheus Silva Franca <matheussfranca@live.com>
2023-03-31 15:11:53 -0300

    Concluído: Requisito 11

--- front-end/src/Components/NavBar.js
+++ front-end/src/Components/NavBar.js
@@ -6,7 +6,7 @@ function NavBar() {
       <h1 data-testid="customer_products__element-navbar-link-products">Produtos</h1>
       <h1 data-testid="customer_products__element-navbar-link-orders">Meus Pedidos</h1>
       <h1 data-testid="customer_products__element-navbar-user-full-name">Nome</h1>
-      <h1 button="customer_products__element-navbar-link-logout">Sair</h1>
+      <h1 data-testid="customer_products__element-navbar-link-logout">Sair</h1>
     </div>
   );
 }
