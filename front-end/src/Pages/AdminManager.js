122567538bdb746c6736153f7c9f766887457a13
esdras <esdras12thiago@gmail.com>
2023-04-04 18:09:43 -0300

    Fazendo validacoes da pagina de Admin

--- front-end/src/Pages/AdminManager.js
+++ front-end/src/Pages/AdminManager.js
@@ -4,25 +4,71 @@ import NavBar from '../Components/NavBar';
 
 function AdminManager() {
   const [users, setUsers] = useState([]);
+  const [state, setState] = useState({
+    email: '',
+    role: 'customer',
+    password: '',
+    name: '',
+  });
+  const [stateBtn, setStateBtn] = useState(true);
+
   const getUsers = async () => {
     const { data } = await genericRoutes('seller/users', 'get');
     setUsers(data);
-    console.log(data);
+  };
+
+  const onChangeForms = ({ target: { value, name } }) => {
+    setState({
+      ...state,
+      [name]: value,
+    });
+  };
+
+  const verifyValues = () => {
+    const emailRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+    const nameMin = 11;
+    const numberMin = 5;
+    const validateEmail = state.email.match(emailRegex);
+    const validatePassword = state.password.length > numberMin;
+    const validateName = state.name.length > nameMin;
+    if (validateEmail && validatePassword && validateName) return setStateBtn(false);
+    return setStateBtn(true);
   };
 
   useEffect(() => {
     getUsers();
   }, []);
 
+  useEffect(() => {
+    verifyValues();
+  });
+
   return (
     <div>
       <NavBar />
       <h1>Cadastrar Usuario</h1>
-      <input type="name" data-testid="admin_manage__input-name" />
-      <input type="name" data-testid="admin_manage__input-email" />
-      <input type="name" data-testid="admin_manage__input-password" />
+      <input
+        type="text"
+        data-testid="admin_manage__input-name"
+        name="name"
+        onChange={ onChangeForms }
+      />
+      <input
+        type="text"
+        data-testid="admin_manage__input-email"
+        name="email"
+        onChange={ onChangeForms }
+      />
+      <input
+        type="password"
+        data-testid="admin_manage__input-password"
+        name="password"
+        onChange={ onChangeForms }
+      />
       <select
         data-testid="admin_manage__select-role"
+        name="role"
+        onChange={ onChangeForms }
       >
         <option value="Vendedor">Vendedor</option>
         <option value="Admin">Admin</option>
@@ -30,6 +76,7 @@ function AdminManager() {
       </select>
       <button
         type="button"
+        disabled={ stateBtn }
         data-testid="admin_manage__button-register"
       >
         Cadastrar
