0d3c865ed6808ff9478cbb3897eb0c607d023763
leonardobrindeiro <leonardo_brindeiro@yahoo.com.br>
2023-04-05 19:50:51 -0300

    req 30 e 31

--- front-end/src/Pages/SellerOrdersDetails.js
+++ front-end/src/Pages/SellerOrdersDetails.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import NavBar from '../Components/NavBar';
+import SellersOrdersDetails from '../Components/SellersOrdersDetails';
+import { genericRoutes } from '../Axios/AxiosRoutes';
 
 function SellerOrdersDetails() {
+  const [sales, setSales] = useState([]);
+
+  const getSales = async () => {
+    const { data } = await genericRoutes('sales', 'get');
+
+    if (sales) {
+      setSales(data);
+      return sales;
+    }
+  };
+
+  useEffect(() => {
+    getSales();
+  }, []);
+
   return (
-    <div />
+    <div>
+      <NavBar />
+      {sales.map((sale) => (
+        <SellersOrdersDetails
+          key={ sale.id }
+          id={ sale.id }
+          status={ sale.status }
+          saleDate={ sale.saleDate }
+          totalPrice={ sale.totalPrice }
+          qtd={ sale.qtd }
+          priceUnit={ sale.priceUnit }
+
+        />
+      ))}
+    </div>
   );
 }
 
